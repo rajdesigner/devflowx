24a2a5ca769ada91071f2e6e6b61bb812459eb98
Rajmani Prasad <rajmani@keepspace.com.au>
2026-05-17 08:09:15 +0530

    Icons, Dev settings like ESLINT, PRettier etc

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 const page = () => {
-  return (
-    <div className='text-5xl'>Welcome to Next.JS Application.</div>
-  )
-}
+  return <div className="text-5xl">Welcome to Next.JS Application.</div>;
+};
 
-export default page
+export default page;
